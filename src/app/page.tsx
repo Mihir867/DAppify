3f573171903d8f40d6eb7cf0e59cb5e72cec3239
Mihir Thakur <Mihirkantthakur867@gmail.com>
2024-05-31 23:19:40 +0530

    Added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
+// Home.js
 
 import Image from "next/image";
 import Navbar from "@/components/Navbar/Navbar"
+import { HeroItem } from "@/components/Hero"
 
 export default function Home() {
   return (
     <>
-    
-    <Navbar />
+      <Navbar />
+      <HeroItem />
     </>
   );
 }
